2a744b90fbadf779fa4543b9e09ff596a2ffca25
taha-khojasteh <tahakhojastehsany@gmail.com>
2022-05-24 20:23:13 +0430

    Set changes

--- App.tsx
+++ App.tsx
@@ -16,9 +16,9 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        {/*<Navigation colorScheme={colorScheme} />*/}
+        <Navigation colorScheme={colorScheme} />
         <StatusBar />
-        <ApplyFormUser/>
+        {/*<ApplyFormUser/>*/}
       </SafeAreaProvider>
     );
   }
